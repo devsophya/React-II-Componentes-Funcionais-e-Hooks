8c10db8c1d728bcbd06bdad93f10296964c6768c
Sophya <sophyadamiazodev@gmail.com>
2025-06-23 11:44:57 -0300

    feat: Styled components using SCSS modules for better organization. Configured TypeScript for strict type checking and module resolution.

---
+++ todo-list/src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { StatsCard } from "../StatsCard/StatsCard";
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>My ToDo</h1>
+
+          <span>Bem-vinda, Sophya!</span>
+        </div>
+
+        <div>
+          <StatsCard title="Total de Tarefas" value={6} />
+          <StatsCard title="Tarefas Pendentes" value={4} />
+          <StatsCard title="Tarefas Concluídas" value={2} />
+        </div>
+      </div>
+    </header>
+  );
+};
