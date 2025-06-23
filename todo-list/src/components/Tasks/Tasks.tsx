6cc4e8ce790e1fd279591c859e64861b55c464ea
Sophya <sophyadamiazodev@gmail.com>
2025-06-23 20:13:50 -0300

    feat: Built a task manager using functional React with state hooks, form handling, and styling.

---
+++ todo-list/src/components/Tasks/Tasks.tsx
@@ -0,0 +1,60 @@
+import { FormEvent, useState } from "react";
+import styles from "./styles.module.scss";
+
+interface Task {
+  title: string;
+  done: boolean;
+  id: number;
+}
+
+export const Tasks: React.FC = () => {
+  const [taskTitle, setTasktitle] = useState("");
+  const [tasks, setTasks] = useState([] as Task[]);
+
+  //Função disparada quando o usuário está querendo adicionar uma nova tarefa
+  function handleSubmitAddTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (taskTitle.length < 3) {
+      alert("Não é possível adicionar uma tarefa com menos de 3 letras.");
+      return;
+    }
+
+    //adicione a tarefa
+    setTasks([
+      ...tasks, //pega todas as tarefas que já existam e coloca nesse novo valor do estado da tarefa
+      { id: new Date().getTime(), title: taskTitle, done: false },
+    ]);
+    setTasktitle("");
+  }
+
+  return (
+    <section className={styles.container}>
+      <form onSubmit={handleSubmitAddTask}>
+        <div>
+          <label htmlFor="task-title">Adicionar Tarefa</label>
+          <input
+            value={taskTitle}
+            onChange={(event) => setTasktitle(event.target.value)}
+            type="text"
+            id="task-title"
+            placeholder="Titulo da Tarefa"
+          />
+        </div>
+
+        <button type="submit">Adicionar Tarefa</button>
+      </form>
+
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type="checkbox" id={`task-${task.id}`} />
+              <label htmlFor={`task-${task.id}`}>{task.title}</label>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
